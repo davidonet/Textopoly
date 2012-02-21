72596ad6abbc19e7fb6d2887892e7fc2a32f27e2
zakxxi <adrien@startxxi.eu>
2012-02-22 06:56:04 +0800

    prototype fonctionnel
    ajout des changements dynamiques de style
    interdiction de retour à la ligne

--- sandbox/switchTypeLive/switchLiveType.js
+++ sandbox/switchTypeLive/switchLiveType.js
@@ -8,19 +8,46 @@ $(document).ready(function() {
 		$('div#charCount').html(charLength + ' / 600');
 
 		// 600
-		if($(this).val().length > 600)
-			$('div#charCount').html('<strong>600 max</strong>');
-			
+		/*if($(this).val().length > 600) {
+		$('div#charCount').html('<strong>600 max</strong>');
+		}*/
+
 		// change le style // WORK IN PROGRESS
 
-		if($(this).val().length >=0 && $(this).val().length <= 3)
+		if($(this).val().length >= 0 && $(this).val().length <= 3) {
 			$('textarea#switchTypeLive').addClass('format1');
-			
-		else if($(this).val().length >=4 && $(this).val().length <= 6)
+			$('textarea#switchTypeLive').removeClass('format2');
+		} else if($(this).val().length >= 4 && $(this).val().length <= 49) {
 			$('textarea#switchTypeLive').addClass('format2');
-			
-		else if($(this).val().length >=7 && $(this).val().length <= 9)
-			$('textarea#switchTypeLive').addClass('format2');
-			
+			$('textarea#switchTypeLive').removeClass('format1');
+			$('textarea#switchTypeLive').removeClass('format3');
+		} else if($(this).val().length >= 50 && $(this).val().length <= 149) {
+			$('textarea#switchTypeLive').addClass('format3');
+			$('textarea#switchTypeLive').removeClass('format2');
+			$('textarea#switchTypeLive').removeClass('format4');
+		} else if($(this).val().length >= 150 && $(this).val().length <= 299) {
+			$('textarea#switchTypeLive').addClass('format4');
+			$('textarea#switchTypeLive').removeClass('format3');
+			$('textarea#switchTypeLive').removeClass('format5');
+
+		} else if($(this).val().length >= 300 && $(this).val().length <= 599) {
+			$('textarea#switchTypeLive').addClass('format5');
+			$('textarea#switchTypeLive').removeClass('format4');
+
+		} else {
+		}
+	});
+	// interdiction linebreak
+
+	$('textarea#switchTypeLive').keypress(function(event) {
+		if(event.keyCode == 13) {
+			event.preventDefault();
+		}
+	});
+
+	$('textarea#switchTypeLive').keyup(function() {
+		var txt = $('textarea#switchTypeLive').val();
+		$('textarea#switchTypeLive').val(txt.replace(/[\n\r]+/g, " "));
+
 	});
 });
