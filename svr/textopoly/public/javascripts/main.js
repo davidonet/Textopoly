f56e8e8093dd74f69c659014acaccfe4b2b3ca9e
zakxxi <adrien@startxxi.eu>
2012-03-16 13:22:06 +0100

    [#26273335] including jquery ui touch punch

--- svr/textopoly/public/javascripts/main.js
+++ svr/textopoly/public/javascripts/main.js
@@ -1,4 +1,4 @@
-require(["jquery","https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"], function($) {
+require(["jquery", "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"], function($) {
 	$(function() {
 		$(document).ready(function() {
 			// masque les infos de debug
@@ -9,12 +9,11 @@ require(["jquery","https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-
 			require(["gridinteraction"], function() {
 				anchorPoint();
 				$('.fz > p').hide();
-			
+
 			});
-			
-			// drag map
-			$('div#map').draggable({
-				appendTo : 'body'
+			require(["jquery.ui.touch-punch.js", "jquery.ui.touch-punch.js"], function(draggable) {
+				// drag map
+				$('div#map').draggable();
 			});
 			// blinking ;-)
 			function blink() {
