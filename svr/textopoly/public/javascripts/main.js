6722ccedbb76e60dc3d7499fb9cee9ff648d58e6
zakxxi <adrien@startxxi.eu>
2012-03-16 12:29:09 +0100

    [#26549937] Réorganisation du code css

--- svr/textopoly/public/javascripts/main.js
+++ svr/textopoly/public/javascripts/main.js
@@ -2,18 +2,29 @@ require(["jquery","https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-
 	$(function() {
 		$(document).ready(function() {
 			// masque les infos de debug
-			$('.coord').hide();
+			//$('.coord').hide();
+			
 			require(["bookingsocket"], function() {
 			});
 			require(["gridinteraction"], function() {
 				anchorPoint();
-				$('.fz > p').hide();
+				//$('.fz > p').hide();
 			});
+			
 			// drag map
 			$('div#map').draggable({
 				appendTo : 'body'
 			});
+			// blinking ;-)
+			function blink() {
+				$("#blink").fadeTo(1200, 0).fadeTo(800, 1, function() {
+					blink()
+				});
+			}
 
+			$(function() {
+				blink();
+			});
 		});
 	});
 });
