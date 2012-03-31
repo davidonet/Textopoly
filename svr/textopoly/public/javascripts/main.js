07abac5a4c7b7dd509caac6698724d65a19daa30
zakxxi <adrien@startxxi.eu>
2012-03-31 17:07:51 +0200

    [#25759057] AJAXify writingForm

--- svr/textopoly/public/javascripts/main.js
+++ svr/textopoly/public/javascripts/main.js
@@ -1,4 +1,4 @@
-require(["jquery", "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js", "jquery.ui.touch-punch"], function($) {
+require(["jquery", "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js", "jquery.ui.touch-punch", "jquery.form"], function($) {
 	$(function() {
 		$(document).ready(function() {
 
@@ -11,28 +11,45 @@ require(["jquery", "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery
 
 				// Insère formulaire d'écriture
 
-				var writingForm = '<div id="writingForm"><h2>Insert a new text</h2><form action="/insert"  method="post"><div><textarea name="t" rows="9" maxlength="600" cols="70"></textarea></div><div><label>X</label><input name="x" type="number" min="-100" max="100"/><label>Y</label><input name="y" type="number" min="-100" max="100"/><label>Size</label><select name="s"><option value="s">Small</option><option value="l">Long</option><option value="t">Tall</option><option value="f">Fat</option></select><label>Author</label><select name="a"><option>davidonet</option><option>zakxxi</option></select><label>Color</label><select name="c" value="butter"><option>butter</option><option>orange</option><option>chocolate</option><option>chameleon</option><option>skyblue</option><option>plum</option><option>scarletred</option></select><input type="submit" value="Insert" /></div></form></div>'
+				var writingForm = '<div id="writingBox"><h2>Insert a new text</h2><form id = "writingForm" action="/insert"  method="post"><div><textarea name="t" rows="9" maxlength="600" cols="70"></textarea></div><div><input name="x" type="number" min="-100" max="100"/><input name="y" type="number" min="-100" max="100"/><label>Size</label><select name="s"><option value="s">Small</option><option value="l">Long</option><option value="t">Tall</option><option value="f">Fat</option></select><label>Author</label><select name="a"><option>davidonet</option><option>zakxxi</option></select><label>Color</label><select name="c" value="butter"><option>butter</option><option>orange</option><option>chocolate</option><option>chameleon</option><option>skyblue</option><option>plum</option><option>scarletred</option></select><input type="submit" value="Insert" /></div></form></div>'
 				$('#map').append(writingForm);
 
+				// AJAXifie le formulaire d'écriture
+
+				$('#writingForm').ajaxForm(function() {
+					alert("Thank you for post");
+					location.reload();
+
+				});
+				// cache les champs X et Y du formulaire d'écriture
+				$('input[name*="x"]').hide();
+				$('input[name*="y"]').hide();
 				// écrire
 				$('.fz').on('click', function(event) {
+
 					var dc = $(this).attr('dc').split(',');
+					// récupère la propriété dc d'un élément .fz dans un tableau
 					var xGrid = dc[0];
+					// récupère x de dc
 					var yGrid = dc[1];
+					// récupère y de dc
+					console.log('xGrid= ' + xGrid + ' | yGrid= ' + yGrid);
 
 					var position = $(this).position();
+					// récupère la position absolue d'un élément .fz
 					var xPos = position.left;
 					var yPos = position.top;
-					console.log('xGrid= ' + xGrid + ' | yGrid= ' + yGrid);
 					console.log('xPos= ' + xPos + ' | yPos= ' + yPos);
 
-					$('#writingForm').css({
+					// positionnement du formulaire d'écriture
+					$('#writingBox').css({
 						'left' : xPos,
 						'top' : yPos
 					});
-					
-					$('input[name$="x"]').val(xGrid);
-					$('input[name$="y"]').val(yGrid)
+
+					// ajoute les coordonnées X Y dans le formulaire
+					$('input[name*="x"]').val(xGrid);
+					$('input[name*="y"]').val(yGrid);
 				});
 			});
 			// slider
