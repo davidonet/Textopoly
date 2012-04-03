c947376c62cf7528968ea94cab3ff189b54fcd17
zakxxi <adrien@startxxi.eu>
2012-04-03 22:07:18 +0200

    writingBox alpha again

--- sandbox/writingBox0.0.0/writingBox.js
+++ sandbox/writingBox0.0.0/writingBox.js
@@ -1,3 +1,21 @@
 /**
  * @author Adrien Revel
  */
+
+$(document).ready(function() {
+
+	$(function() {
+
+		$("#writingArea").resizable({
+			handles : 'e, s, se',
+			grid : [240, 160],
+			minWidth : 240,
+			maxWidth : 480,
+			minHeight : 160,
+			maxHeight : 320,
+			alsoResize : '#writingBox'
+		});
+		$("#writingBox").resizable();
+
+	});
+});
---
+++ sandbox/writingBox0.1.0/writingBox.js
@@ -0,0 +1,56 @@
+/**
+ * @author Adrien Revel
+ */
+
+$(document).ready(function() {
+
+	$('#size').change(function() {
+		var size = $('#size option:selected').val();
+		switch(size) {
+			case 's':
+				$('#writingArea').removeClass('l t f').addClass('s');
+				break;
+			case 'l':
+				$('#writingArea').removeClass('s t f').addClass('l');
+				break;
+			case 't':
+				$('#writingArea').removeClass('l s f').addClass('t');
+				break;
+			case 'f':
+				$('#writingArea').removeClass('l t s').addClass('f');
+				break;
+
+		};
+
+	})
+
+	$('#color').css('backgroundColor', '#edd400')
+
+	$('#color').change(function() {
+		var color = $('#color option:selected').val();
+		switch(color) {
+			case 'butter':
+				$('#color').css('backgroundColor', '#edd400')
+				break;
+			case 'orange':
+				$('#color').css('backgroundColor', '#f57900')
+				break;
+			case 'chocolate':
+				$('#color').css('backgroundColor', '#c17d11')
+				break;
+			case 'chameleon':
+				$('#color').css('backgroundColor', '#73d216')
+				break;
+			case 'skyblue':
+				$('#color').css('backgroundColor', '#3465a4')
+				break;
+			case 'plum':
+				$('#color').css('backgroundColor', '#75507b')
+				break;
+			case 'scarletred':
+				$('#color').css('backgroundColor', '#cc0000')
+				break;
+		};
+
+	})
+});
