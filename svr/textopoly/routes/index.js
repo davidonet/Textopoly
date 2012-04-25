79eacd359d1296a37a83dbb11a3d2daf5da64268
David Olivari <dolivari@davidoDesktop.(none)>
2012-04-25 17:31:47 +0200

    [#28591203] Cleaning code and implement dynamic reload when dragging outside view range

--- svr/textopoly/routes/index.js
+++ svr/textopoly/routes/index.js
@@ -6,7 +6,6 @@ var models = require('./models/mongodrv');
 var mapC = require('./controllers/map');
 var imgC = require('./controllers/img');
 var jsonC = require('./controllers/json');
-var dmC = require('./controllers/dynmap_engine');
 
 module.exports = function(app) {
 	app.get('/', mapC.view);
@@ -16,7 +15,4 @@ module.exports = function(app) {
 	app.get('/section', jsonC.section);
 	app.post('/insert', jsonC.insert);
 	app.get('/remove', jsonC.remove);
-	app.get('/block', dmC.block);
-	app.get('/dynmap', dmC.dynmap);
-	app.get('/get', dmC.get);
 }
