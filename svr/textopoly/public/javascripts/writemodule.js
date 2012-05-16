d00f3f1107e752bb905fa36218ad53ac196238fe
zakxxi <adrien@startxxi.eu>
2012-05-16 11:15:25 +0200

    re-organizing writemodule

--- svr/textopoly/public/javascripts/writemodule.js
+++ svr/textopoly/public/javascripts/writemodule.js
@@ -1,21 +1,32 @@
-require(["freeadjacent","lib/fileuploader"], function(freeAdjacent,fileUploader) {
+require(["freeadjacent", "lib/fileuploader"], function(freeAdjacent, fileUploader) {
+
 	var delay = 250
 	var textarea = true
 	var auth = ($.cookie("author") != null);
 	var isFatFree = false;
-	// Fonction reinitialise le formulaire d'écriture
+	var uploader = new qq.FileUploader({
+		element : $('.imageArea')[0],
+		action : '/postimg',
+		debug : false,
+		onComplete : function() {
+			var dc = $('#writingBox').attr('dc').split(',');
+			$(location).attr('href', '/view?zoom=2&xcenter=' + dc[0] + '&ycenter=' + dc[1]);
+		}
+	});
 
+	/***********************************************************************************
+	 * BEGIN RESET
+	 ***********************************************************************************/
+
+	// Reset writingBox
 	function resetWritingBox() {
 		$('.editArea').switchClass('l t f', 's', delay, function() {
 			handlesPos();
 		});
-
 		$('.editArea').addClass('l4').removeClass('l15 l50 l150 l300 l600');
-
 		$('.e.handle').switchClass('al', 'ar', 0)
 		$('.s.handle').switchClass('au', 'ad', 0)
 		$('.sw.handle').switchClass('tx', 'me', 0)
-
 		$('.sw.handle').show();
 		$('.e.handle').show();
 		$('.s.handle').show();
@@ -28,65 +39,15 @@ require(["freeadjacent","lib/fileuploader"], function(freeAdjacent,fileUploader)
 		textarea = true
 	};
 
-	// Écrire
-	$('.z2 > .fz').on('click', function(event) {
-		$('#writingBox').fadeIn(delay);
-		$('textarea[name*=t]').focus();
-		$('.editArea').switchClass('l t f', 's', delay, function() {
-			handlesPos();
-		});
+	// Reset infoBox
 
-		$('.e.handle').switchClass('al', 'ar', 0)
-		$('.s.handle').switchClass('au', 'ad', 0)
-		$('.sw').switchClass('tx', 'me', 0)
+	/***********************************************************************************
+	 * END RESET
+	 ***********************************************************************************/
 
-		var dc = $(this).attr('dc').split(',');
-		$('#writingBox').attr('dc', $(this).attr('dc'));
-		// récupère la propriété dc d'un élément .fz dans un tableau
-		var xGrid = dc[0];
-		// récupère x de dc
-		var yGrid = dc[1];
-		// récupère y de dc
-		var position = $(this).position();
-		// récupère la position absolue d'un élément .fz
-		var xPos = position.left;
-		var yPos = position.top;
-
-		// récupère les cases libres autour
-		var fA = (freeAdjacent(xGrid, yGrid));
-
-		$('.e.handle').hide();
-		$('.s.handle').hide();
-
-		$(fA).each(function(index, value) {
-
-			if(value == 'e')
-				$('.e.handle').show();
-			if(value == 's')
-				$('.s.handle').show();
-			if(value == 'se')
-				isFatFree = true;
-		});
-
-		// active les posibilités de tailles
-		/*
-		$('#e').hide();
-		$('#se').hide();
-		$('#s').hide();
-		$.each(fA, function(index, value) {
-		$('#' + value).show();
-		});
-		*/
-
-		// positionnement du formulaire d'écriture
-		$('#writingBox').animate({
-			'left' : parseInt(xPos - 10),
-			'top' : parseInt(yPos - 10),
-		}, delay);
-
-	});
-
-	// position des poignées
+	/***********************************************************************************
+	 * BEGIN HANDLEPOS
+	 ***********************************************************************************/
 
 	function handlesPos() {
 		var r = 10;
@@ -134,8 +95,73 @@ require(["freeadjacent","lib/fileuploader"], function(freeAdjacent,fileUploader)
 		});
 	}
 
-	// change la taille du formulaire
+	/***********************************************************************************
+	 * END HANDLEPOS
+	 ***********************************************************************************/
 
+
+
+
+	/***********************************************************************************
+	 * BEGIN WRITINGBOX
+	 ***********************************************************************************/
+
+	$('.z2 > .fz').on('click', function(event) {
+		$('#writingBox').fadeIn(delay);
+		$('textarea[name*=t]').focus();
+		$('.editArea').switchClass('l t f', 's', delay, function() {
+			handlesPos();
+		});
+
+		$('.e.handle').switchClass('al', 'ar', 0)
+		$('.s.handle').switchClass('au', 'ad', 0)
+		$('.sw').switchClass('tx', 'me', 0)
+
+		var dc = $(this).attr('dc').split(',');
+		$('#writingBox').attr('dc', $(this).attr('dc'));
+		// récupère la propriété dc d'un élément .fz dans un tableau
+		var xGrid = dc[0];
+		// récupère x de dc
+		var yGrid = dc[1];
+		// récupère y de dc
+		var position = $(this).position();
+		// récupère la position absolue d'un élément .fz
+		var xPos = position.left;
+		var yPos = position.top;
+
+		// récupère les cases libres autour
+		var fA = (freeAdjacent(xGrid, yGrid));
+
+		$('.e.handle').hide();
+		$('.s.handle').hide();
+
+		$(fA).each(function(index, value) {
+
+			if(value == 'e')
+				$('.e.handle').show();
+			if(value == 's')
+				$('.s.handle').show();
+			if(value == 'se')
+				isFatFree = true;
+		});
+
+		// positionnement du formulaire d'écriture
+		$('#writingBox').animate({
+			'left' : parseInt(xPos - 10),
+			'top' : parseInt(yPos - 10),
+		}, delay);
+
+	});
+
+	/***********************************************************************************
+	 * END WRITINGBOX
+	 ***********************************************************************************/
+
+	/***********************************************************************************
+	 * BEGIN INTERACTIONS ON WRITINGBOX HANDLES
+	 ***********************************************************************************/
+
+	// EAST
 	$('.e.handle').click(function() {
 		$(this).hide()
 		if(!isFatFree)
@@ -175,6 +201,7 @@ require(["freeadjacent","lib/fileuploader"], function(freeAdjacent,fileUploader)
 
 	});
 
+	// SOUTH
 	$('.s.handle').click(function() {
 		$(this).hide()
 		if(!isFatFree)
@@ -213,15 +240,12 @@ require(["freeadjacent","lib/fileuploader"], function(freeAdjacent,fileUploader)
 
 	});
 
-	// actions sur les poignées
-
+	// NORTH WEST
 	$('.nw.handle').click(function() {
 		resetWritingBox();
-
 	})
-
+	// SOUTH EST
 	$('.se.handle').click(function() {
-
 		if(auth == false) {
 			$('.sw.handle').hide();
 			$('.e.handle').hide();
@@ -230,9 +254,7 @@ require(["freeadjacent","lib/fileuploader"], function(freeAdjacent,fileUploader)
 			$('textarea[name*=t]').hide();
 			$('.authorArea').show();
 			auth = true
-
 		} else {
-
 			var dc = $('#writingBox').attr('dc').split(',');
 			var aSize = 's';
 			if($('.editArea').hasClass('l'))
@@ -259,26 +281,16 @@ require(["freeadjacent","lib/fileuploader"], function(freeAdjacent,fileUploader)
 				},
 				dataType : 'json'
 			});
-
-		}
-
-	});
-
-	var uploader = new qq.FileUploader({
-		element :$('.imageArea')[0],
-		action : '/postimg',
-		debug : false,
-		onComplete: function(){
-			var dc = $('#writingBox').attr('dc').split(',');
-			$(location).attr('href', '/view?zoom=2&xcenter=' + dc[0] + '&ycenter=' + dc[1]);
 		}
 	});
+
+	// SOUTH WEST
 	$('.sw.handle').click(function() {
 		if(textarea == true) {
 			$('textarea[name*=t]').val('');
 			$('.editArea').addClass('l4').removeClass('l15 l50 l150 l300 l600');
 			$('textarea[name*=t]').hide();
-			
+
 			$('.imageArea').show();
 			var dc = $('#writingBox').attr('dc').split(',');
 			var aSize = 's';
@@ -308,27 +320,19 @@ require(["freeadjacent","lib/fileuploader"], function(freeAdjacent,fileUploader)
 
 	});
 
-	// SWITCH LIVE TYPE
+	/***********************************************************************************
+	 * END INTERACTIONS ON WRITINGBOX HANDLES
+	 ***********************************************************************************/
+
+	/***********************************************************************************
+	 * BEGIN SWITCHLIVETYPE
+	 ***********************************************************************************/
 
 	var wT = 'textarea[name*=t]'
 	var wA = '.editArea'
 	// controls character input/counter
 	$(wT).keyup(function() {
 		var charLength = $(this).val().length;
-
-		// Afficher le nombre de caractères
-		$('div#charCount').html(charLength + ' / 600');
-
-		// 600
-		/*
-		if($(this).val().length > 600) {
-		$('div#charCount').html('<strong>600 max</strong>');
-		}
-		*/
-
-		// change le style
-
-		//
 
 		if($(this).val().length >= 0 && 3 >= $(this).val().length) {
 			$(wA).addClass('l4').removeClass('l15 l50 l150 l300 l600');
@@ -351,15 +355,6 @@ require(["freeadjacent","lib/fileuploader"], function(freeAdjacent,fileUploader)
 		}
 	});
 
-	// interdiction linebreak
-	/*
-	$(wT).keypress(function(event) {
-	if(event.keyCode == 13) {
-	event.preventDefault();
-	}
-	});
-	*/
-
 	// Remplace les sauts de ligne par des espaces
 
 	$(wT).keyup(function() {
@@ -368,4 +363,7 @@ require(["freeadjacent","lib/fileuploader"], function(freeAdjacent,fileUploader)
 
 	});
 });
-// SWITCH LIVE TYPE END
+
+/***********************************************************************************
+ * END SWITCHLIVETYPE
+ ***********************************************************************************/
