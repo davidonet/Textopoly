d2ee3d29011f5c70c75475aa409cf09d185ca159
zakxxi <adrien@startxxi.eu>
2012-05-16 21:28:27 +0200

    adding tipsy lib

--- svr/textopoly/public/javascripts/main.js
+++ svr/textopoly/public/javascripts/main.js
@@ -3,7 +3,7 @@ requirejs.config({
 		'jquery-ui' : 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min',
 	}
 });
-require(["jquery", "jquery-ui", "lib/jquery.ui.touch-punch", "lib/jquery.form", "lib/syronex-colorpicker"], function($) {
+require(["jquery", "jquery-ui", "lib/jquery.ui.touch-punch", "lib/jquery.form", "lib/syronex-colorpicker", "lib/jquery.tipsy"], function($) {
 	$(function() {
 		$(document).ready(function() {
 
