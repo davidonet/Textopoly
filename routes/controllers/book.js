86ebd04b24524b25ecfe044ee004ba79e191b6c7
David Olivari <david.olivari@gmail.com>
2012-09-04 16:53:31 +0200

    Adding text length class

--- routes/controllers/book.js
+++ routes/controllers/book.js
@@ -1,5 +1,31 @@
+function txtLen2Class(txtlen) {
+	var lclass = '';
+	if (txtlen < 1) {
+		lclass = 'l0';
+	} else if (txtlen < 4) {
+		lclass = 'l4';
+	} else if (txtlen < 15) {
+		lclass = 'l15';
+	} else if (txtlen < 50) {
+		lclass = 'l50';
+	} else if (txtlen < 150) {
+		lclass = 'l150';
+	} else if (txtlen < 300) {
+		lclass = 'l300';
+	} else if (txtlen < 601) {
+		lclass = 'l600';
+	} else {
+		lclass = 'warning';
+	}
+	return lclass;
+}
+
 exports.path = function(req, res) {
 	db.path.expand(req.params.id, function(err, items) {
+		
+		items.pw.forEach(function(item,idx){
+			items.pw[idx].lc = txtLen2Class(item.t.length);
+		});
 		res.render('book.jade', {
 			title : "Textopoly | Chemin de " + items.a,
 			a : items.a,
@@ -25,8 +51,9 @@ exports.txt = function(req, res) {
 		y : req.params.y
 	}, function(err, items) {
 		res.render('txt.jade', {
-			title : "Textopoly | "+req.params.x+","+req.params.y,
+			title : "Textopoly | " + req.params.x + "," + req.params.y,
 			t : items,
+			lc : txtLen2Class(items.t.length),
 			layout : 'mobile'
 		});
 	});
@@ -35,7 +62,6 @@ exports.txt = function(req, res) {
 exports.choice = function(req, res) {
 	db.path.fromAuth(req.params.a, function(err, items) {
 		db.txt.authorTxt(req.params.a, function(err, txts) {
-			console.log(txts);
 			res.render('choice.jade', {
 				title : "Textopoly | Chemins de " + req.params.a,
 				a : req.params.a,
