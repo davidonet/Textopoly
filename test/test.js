68ceee37a8b59e7236c47322d00d2e05779d14ad
David Olivari <david.olivari@gmail.com>
2012-09-05 09:07:05 +0200

    new txt Test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('superagent');
 var app = require('../app');
 var should = require('should');
 
-function testTxt(txt){
+function testTxt(txt) {
 	/*jshint evil:true,es5: true*/
 	txt.should.have.property("a").and.be.a('string');
 	txt.should.have.property("c").and.be.a('string');
@@ -16,7 +16,6 @@ describe('My Server', function() {
 		it("should respond with a list of txt", function(done) {
 			request.get('http://localhost:3000/section?xmin=-5&xmax=5&ymin=-5&ymax=5').set('Accept', 'application/json').end(function(res) {
 				should.exist(res.body);
-				var success = res.body.success;
 				res.body.should.have.property("success").equal(true);
 				res.body.should.have.property("xmin");
 				res.body.should.have.property("xmax");
@@ -25,6 +24,15 @@ describe('My Server', function() {
 				res.body.should.have.property("texts");
 				res.body.texts.length.should.be.above(2);
 				testTxt(res.body.texts[0]);
+				done();
+			});
+		});
+	});
+	describe('GET /t', function() {
+		it("should respond a txt", function(done) {
+			request.get('http://localhost:3000/t/3/0').set('Accept', 'application/json').end(function(res) {
+				should.exist(res.body);
+				testTxt(res.body);
 				done();
 			});
 		});
