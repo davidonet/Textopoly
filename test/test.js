dcab6db7219090593fbab84d6a8fd54fbaf30358
David Olivari <david.olivari@gmail.com>
2012-09-05 17:32:37 +0200

    more testcases

--- test/test.js
+++ test/test.js
@@ -7,12 +7,15 @@ function testTxt(txt) {
 	txt.should.have.property("a").and.be.a('string');
 	txt.should.have.property("c").and.be.a('string');
 	txt.should.have.property("d").and.be.a('string');
-	txt.should.have.property("p").with.
-	lengthOf(2);
+	txt.should.have.property("p").and.lengthOf(2);
 	txt.should.have.property("t").and.be.a('string');
 }
 
-describe('My Server', function() {
+function testPath(path) {
+	path.should.have.property("pw");
+}
+
+describe('Textopoly Server Side', function() {
 	describe('GET /section', function() {
 		it("should respond with a list of txt", function(done) {
 			request.get('http://localhost:3000/section?xmin=-5&xmax=5&ymin=-5&ymax=5').set('Accept', 'application/json').end(function(res) {
@@ -29,7 +32,7 @@ describe('My Server', function() {
 			});
 		});
 	});
-	describe('GET /t', function() {
+	describe('GET /t/:x/:y', function() {
 		it("should respond a txt", function(done) {
 			request.get('http://localhost:3000/t/3/0').set('Accept', 'application/json').end(function(res) {
 				should.exist(res.body);
@@ -38,10 +41,36 @@ describe('My Server', function() {
 			});
 		});
 	});
-	describe('GET /fa', function() {
-		it("should respond an array of occupied cells", function(done) {
+	describe('GET /fa/:x/:y', function() {
+		it("should respond a composite object of txts and paths", function(done) {
 			request.get('http://localhost:3000/fa/3/0').set('Accept', 'application/json').end(function(res) {
 				should.exist(res.body);
+				res.body.should.have.property("s");
+				res.body.should.have.property("l");
+				res.body.should.have.property("t");
+				res.body.should.have.property("f");
+				done();
+			});
+		});
+	});
+	describe('GET /a/:a', function() {
+		it("should respond an array of occupied cells", function(done) {
+			request.get('http://localhost:3000/a/Adrien').set('Accept', 'application/json').end(function(res) {
+				should.exist(res.body);
+				res.body.should.have.property("a").and.equal("Adrien");
+				res.body.should.have.property("paths");
+				testPath(res.body.paths[0]);
+				res.body.should.have.property("txts");
+				done();
+			});
+		});
+	});
+	describe('GET /p/:id', function() {
+		it("should respond an array of txts following a path", function(done) {
+			request.get('http://localhost:3000/p/4fc4b9ad4d71520266000120').set('Accept', 'application/json').end(function(res) {
+				should.exist(res.body);
+				res.body.should.have.property("pw");
+				testTxt(res.body.pw[0]);
 				done();
 			});
 		});
