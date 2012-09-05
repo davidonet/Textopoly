2053fce53fc385b45eedfd881c984855538c48ef
David Olivari <david.olivari@gmail.com>
2012-09-05 07:38:07 +0200

    Adding Behavior-driven development test case with mocha

---
+++ test/test.js
@@ -0,0 +1,32 @@
+var request = require('superagent');
+var app = require('../app');
+var should = require('should');
+
+function testTxt(txt){
+	/*jshint evil:true,es5: true*/
+	txt.should.have.property("a").and.be.a('string');
+	txt.should.have.property("c").and.be.a('string');
+	txt.should.have.property("d").and.be.a('string');
+	txt.should.have.property("p").with.lengthOf(2);
+	txt.should.have.property("t").and.be.a('string');
+}
+
+describe('My Server', function() {
+	describe('GET /section', function() {
+		it("should respond with a list of txt", function(done) {
+			request.get('http://localhost:3000/section?xmin=-5&xmax=5&ymin=-5&ymax=5').set('Accept', 'application/json').end(function(res) {
+				should.exist(res.body);
+				var success = res.body.success;
+				res.body.should.have.property("success").equal(true);
+				res.body.should.have.property("xmin");
+				res.body.should.have.property("xmax");
+				res.body.should.have.property("ymin");
+				res.body.should.have.property("ymax");
+				res.body.should.have.property("texts");
+				res.body.texts.length.should.be.above(2);
+				testTxt(res.body.texts[0]);
+				done();
+			});
+		});
+	});
+});
