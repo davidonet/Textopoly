9aced187fec179a0a0352e2453089846a7b7d897
David Olivari <david.olivari@gmail.com>
2012-09-26 14:03:20 +0200

    reorganizing pathMode init and leave

--- public/javascripts/pathMode.js
+++ public/javascripts/pathMode.js
@@ -1,34 +1,23 @@
 define(["txt", "uievent", "pathWalk", "pathBox", "mapModule", "helper"], function(txt, uievent, pathWalk, pathBox, mapModule, helper) {
 	return {
 		init : function() {
-/*
-			require(["modeHandler"], function(modeHandler) {
-           		modeHandler.refresh();
-            );
- */
-			mapModule.zoomTo(4);
 			uievent.unbindZoom();
+			mapModule.zoomTo(4);
 			$('#zoomWrap').fadeOut(250);
 			pathBox.bindMsg();
-
-
 		},
 		refresh : function(localParams) {
-
-			pathBox.bindMsg();
-
 			var postTxtLoad = function() {
 
 				if ((1 < params.zoom) && (params.zoom < 20)) {
 					//Activate path display
 					pathWalk.updatePath();
+					pathBox.bindMsg();
 				}
-
 			};
 			txt.loadSection(localParams, postTxtLoad);
 		},
 		leave : function() {
-			$('.msg').bind('dblclick');
 			uievent.bindZoom();
 			$('#pathBox').fadeOut(100);
 			$('#zoomWrap').fadeIn(250);
