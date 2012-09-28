2fc6364159f3bee39466d4b6bf6f9090e0ccae1a
David Olivari <david.olivari@gmail.com>
2012-09-28 19:05:27 +0200

    Changing pos of change

--- test/server.js
+++ test/server.js
@@ -27,7 +27,7 @@ function testPath(path) {
 
 describe('Textopoly Server Side', function() {
 	var aTxt = {
-		p : [-7000, 7000],
+		p : [7000, -7000],
 		a : 'mocha',
 		t : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 		c : 'butter',
