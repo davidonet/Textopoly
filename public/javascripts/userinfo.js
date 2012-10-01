d676d19a3f74980b7208332a14aab37f9c71d30a
David Olivari <david.olivari@gmail.com>
2012-10-01 12:02:17 +0200

    color picker disapperance bug

--- public/javascripts/userinfo.js
+++ public/javascripts/userinfo.js
@@ -1,46 +1,50 @@
 define(["lib/syronex-colorpicker"], function() {
+
+	var updateCP = function() {
+		$('#colorPicker').colorPicker({
+			color : new Array("#fce94f", "#fcaf3e", "#8ae234", "#729fcf", "#ad7fa8", "#ef2929"),
+			defaultColor : params.user.color,
+			columns : 6,
+			click : function(color) {
+				switch(color) {
+					case "#fce94f" :
+						params.c = "butter";
+						break;
+					case "#fcaf3e" :
+						params.c = "orange";
+						break;
+					case "#8ae234" :
+						params.c = "chameleon";
+						break;
+					case "#729fcf" :
+						params.c = "skyblue";
+						break;
+					case "#ad7fa8" :
+						params.c = "plum";
+						break;
+					case "#ef2929" :
+						params.c = "scarletred";
+						break;
+				}
+				params.user.color = color;
+			}
+		});
+	};
+
 	var check = function(fn) {
 		if (params.user) {
+			updateCP();
 			fn();
 		} else {
 			$('#loginresult').text("");
 			$('#loginform').ajaxForm({
 				success : function(data) {
 					params.user = data;
+					updateCP();
 					$('#login').hide();
 					$('#footer').animate({
 						bottom : -470
 					}, fn);
-
-					$('#colorPicker').colorPicker({
-						color : new Array("#fce94f", "#fcaf3e", "#8ae234", "#729fcf", "#ad7fa8", "#ef2929"),
-						defaultColor : params.user.color,
-						columns : 6,
-						click : function(color) {
-							switch(color) {
-								case "#fce94f" :
-									params.c = "butter";
-									break;
-								case "#fcaf3e" :
-									params.c = "orange";
-									break;
-								case "#8ae234" :
-									params.c = "chameleon";
-									break;
-								case "#729fcf" :
-									params.c = "skyblue";
-									break;
-								case "#ad7fa8" :
-									params.c = "plum";
-									break;
-								case "#ef2929" :
-									params.c = "scarletred";
-									break;
-							}
-							params.user.color = color;
-						}
-					});
-
 				},
 				error : function() {
 					$('#loginresult').text("Identification invalide");
@@ -54,7 +58,7 @@ define(["lib/syronex-colorpicker"], function() {
 	};
 	return {
 		init : function() {
-
+			updateCP();
 			$("#cancelLogin").click(function() {
 				$('#login').hide();
 				$('#footer').animate({
