d32b2dc51919a68c342f1f9079a7b1dc447853f0
David Olivari <david.olivari@gmail.com>
2012-11-29 20:17:26 +0100

    missing external lib

--- public/javascripts/app.build.js
+++ public/javascripts/app.build.js
@@ -2,11 +2,12 @@
 	appDir : "..",
 	baseUrl : "javascripts",
 	dir : "../../public-optimize",
-	keepBuildDir: false,
+	keepBuildDir : false,
 	paths : {
 		"jquery" : "lib/require-jquery",
 		'jquery-ui' : 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min',
-		"socket.io" : '../../node_modules/socket.io/node_modules/socket.io-client/dist/socket.io'
+		"socket.io" : '../../node_modules/socket.io/node_modules/socket.io-client/dist/socket.io',
+		'stratus' : 'http://stratus.sc/stratus'
 	},
 	modules : [{
 		name : "main",
